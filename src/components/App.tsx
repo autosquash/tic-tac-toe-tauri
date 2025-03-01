2a63df5349a5d8dd2dea966c75bbd4cb6f41d970
autosquash <49131885+autosquash@users.noreply.github.com>
2025-03-01 19:14:22 +0100

    create initial screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import TicTacToe from './TicTacToe'
 
 function App() {
-  return <TicTacToe />
+  const [gameStarted, setGameStarted] = useState(false)
+
+  return gameStarted ? (
+    <TicTacToe />
+  ) : (
+    <div>
+      <button
+        className="start-game-button"
+        onClick={() => setGameStarted(true)}
+      >
+        Empezar
+      </button>
+    </div>
+  )
 }
 
 export default App
